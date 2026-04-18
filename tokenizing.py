0a77eb2a1d181e7a7cf961fb447973962dc08586
Alvaro Rodrigues Vicente <alvarorodriguesvicente2017@gmail.com>
2026-04-17 22:20:19 -0300

    Tokenizando e analisando compressão

---
+++ tokenizing.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from tokenizers import Tokenizer, models, trainers, pre_tokenizers, decoders
+
+
+print('Loading data...')
+df_train = pd.read_csv('data/train.csv')
+
+print('Declaring tokenizer...')
+for vocab_size in [500, 1000, 5000, 10000, 25000, 30000, 50000]:
+    tokenizer = Tokenizer(models.BPE(unk_token="<UNK>"))
+
+    tokenizer.pre_tokenizer = pre_tokenizers.Whitespace()
+    tokenizer.decoder = decoders.WordPiece(prefix="##")
+
+    trainer = trainers.BpeTrainer(
+        vocab_size=vocab_size,
+        min_frequency=2,
+        special_tokens=["<PAD>", "<UNK>", "<CLS>", "<SEP>", "<MASK>"],
+    )
+
+    print('Training tokenizer...')
+    tokenizer.train_from_iterator(
+        df_train['pt'].tolist() + df_train['en'].tolist(),
+        trainer=trainer
+    )
+
+    print('Saving tokenizer...')
+    tokenizer.save(f'artifacts/tokenizer_{vocab_size}.json')
